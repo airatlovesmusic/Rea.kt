f588ec389478eb080fbf90b80cab9507f3499a83
airatlovesmusic <airatlovesmusic@gmail.com>
2020-02-10 00:22:09 +0300

    Добавлен модуль iosApp

---
+++ native/iosApp/iosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by  Airat Khalilov on 10/02/2020.
+//  Copyright © 2020  Airat Khalilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
