4d6b02f03d5b0995b08d434d4ac3d9dffb7c923e
airatlovesmusic <airatlovesmusic@gmail.com>
2020-02-10 00:54:24 +0300

    Проверка связи модуля shared и iosApp

--- native/iosApp/iosApp/ViewController.swift
+++ native/iosApp/iosApp/ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import shared
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Hello from " + GetNameKt.getName()
     }
-
-
+    
 }
 
